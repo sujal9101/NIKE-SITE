68c51cd9efcebde213da3a5b82be6c0a06bf5456
Sujal Gangshettiwar <sujalgangshettiwar9101@gmail.com>
2024-01-20 23:58:27 +0530

    secomd commit

---
+++ src/constants/index.js
@@ -0,0 +1,124 @@
+import { facebook, instagram, shieldTick, support, truckFast, twitter } from "../assets/icons";
+import { bigShoe1, bigShoe2, bigShoe3, customer1, customer2, shoe4, shoe5, shoe6, shoe7, thumbnailShoe1, thumbnailShoe2, thumbnailShoe3 } from "../assets/images";
+
+export const navLinks = [
+    { href: "#home", label: "Home" },
+    { href: "#about-us", label: "About Us" },
+    { href: "#products", label: "Products" },
+    { href: "#contact-us", label: "Contact Us" },
+];
+
+export const shoes = [
+    {
+        thumbnail: thumbnailShoe1,
+        bigShoe: bigShoe1,
+    },
+    {
+        thumbnail: thumbnailShoe2,
+        bigShoe: bigShoe2,
+    },
+    {
+        thumbnail: thumbnailShoe3,
+        bigShoe: bigShoe3,
+    },
+];
+
+export const statistics = [
+    { value: '1k+', label: 'Brands' },
+    { value: '500+', label: 'Shops' },
+    { value: '250k+', label: 'Customers' },
+];
+
+export const products = [
+    {
+        imgURL: shoe4,
+        name: "Nike Running",
+        price: "₹2995",
+    },
+    {
+        imgURL: shoe5,
+        name: "Nike Max",
+        price: "₹5995",
+    },
+    {
+        imgURL: shoe6,
+        name: "Nike Air Dunks",
+        price: "₹4995",
+    },
+    {
+        imgURL: shoe7,
+        name: "Nike NB",
+        price: "₹10,995",
+    },
+];
+
+export const services = [
+    {
+        imgURL: truckFast,
+        label: "Free shipping",
+        subtext: "Enjoy seamless shopping with our complimentary shipping service."
+    },
+    {
+        imgURL: shieldTick,
+        label: "Secure Payment",
+        subtext: "Experience worry-free transactions with our secure payment options."
+    },
+    {
+        imgURL: support,
+        label: "Love to help you",
+        subtext: "Our dedicated team is here to assist you every step of the way."
+    },
+];
+
+export const reviews = [
+    {
+        imgURL: customer1,
+        customerName: 'Anant Mehrotra',
+        rating: 4.8,
+        feedback: "The attention to detail and the quality of the product exceeded my expectations. Highly recommended!"
+    },
+    {
+        imgURL: customer2,
+        customerName: 'Aayushi Sharma',
+        rating: 4.5,
+        feedback: "The product not only met but exceeded my expectations. I'll definitely be a returning customer!"
+    }
+];
+
+
+export const footerLinks = [
+    {
+        title: "Products",
+        links: [
+            { name: "Air Force 1", link: "/" },
+            { name: "Air Max 1", link: "/" },
+            { name: "Air Jordan 1", link: "/" },
+            { name: "Air Max", link: "/" },
+            { name: "Nike Waffle Racer", link: "/" },
+            { name: "Nike Cortez", link: "/" },
+        ],
+    },
+    {
+        title: "Help",
+        links: [
+            { name: "About us", link: "/" },
+            { name: "FAQs", link: "/" },
+            { name: "How it works", link: "/" },
+            { name: "Privacy policy", link: "/" },
+            { name: "Payment policy", link: "/" },
+        ],
+    },
+    {
+        title: "Get in touch",
+        links: [
+            { name: "customer@nike.com", link: "mailto:customer@nike.com" },
+            { name: "+919307736565", link: "tel:+919307736565" },
+        ],
+    },
+];
+
+export const socialMedia = [
+    { src: facebook, alt: "facebook logo" },
+    { src: twitter, alt: "twitter logo" },
+    { src: instagram, alt: "instagram logo" },
+];
